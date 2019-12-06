6cb1169cc01b8070681da25b811fa3763b925e0f
Eduardo Colina <eacolina@edu.uwaterloo.ca>
2019-12-05 23:14:14 -0500

    Split UI into functions

--- ui.go
+++ ui.go
@@ -10,33 +10,14 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-type post struct {
-	username string
-	message  string
-	time     string
+
+type chatView struct{
+	History *tui.Box
+	Input *tui.Entry
+	Chat *tui.Box
 }
 
-var posts = []post{
-	{username: "john", message: "hi, what's up?", time: "14:41"},
-	{username: "jane", message: "not much", time: "14:43"},
-}
-
-func main() {
-
-	fmt.Println("Starting Client...🚀")
-	header := make(http.Header)
-	var Dialer websocket.Dialer
-
-	header.Add("Origin", "http://localhost:3131/")
-
-
-	conn, resp, err := Dialer.Dial("ws://localhost:3434/ws", header)
-
-	if err == websocket.ErrBadHandshake {
-		fmt.Printf("handshake failed with status %d\n", resp.StatusCode)
-		panic(err)
-	}
-
+func (view *chatView) SetupChat(){
 	history := tui.NewVBox()
 
 	historyScroll := tui.NewScrollArea(history)
@@ -55,29 +36,55 @@ func main() {
 
 	chat := tui.NewVBox(historyBox, inputBox)
 	chat.SetSizePolicy(tui.Expanding, tui.Expanding)
+	view.Chat = chat
+	view.History = history
+	view.Input = input
+}
 
-	input.OnSubmit(func(e *tui.Entry) {
+
+func connectToSocket(url string) *websocket.Conn{
+	header := make(http.Header)
+	var Dialer websocket.Dialer
+
+	header.Add("Origin", "http://localhost:3434/")
+
+
+	conn, resp, err := Dialer.Dial(url, header)
+
+	if err == websocket.ErrBadHandshake {
+		fmt.Printf("handshake failed with status %d\n", resp.StatusCode)
+		panic(err)
+	}
+	return conn
+}
+
+
+
+
+func main() {
+
+	fmt.Println("Starting Client...🚀")
+	conn := connectToSocket("ws://localhost:3434/ws")
+	chatView := chatView{}
+	chatView.SetupChat()
+
+
+
+	chatView.Input.OnSubmit(func(e *tui.Entry) {
 		message := []byte(e.Text())
 		conn.WriteMessage(websocket.TextMessage, message)
-		history.Append(tui.NewHBox(
+		chatView.History.Append(tui.NewHBox(
 			tui.NewLabel(time.Now().Format("15:04")),
 			tui.NewPadder(1, 0, tui.NewLabel(fmt.Sprintf("<%s>", "john"))),
 			tui.NewLabel(e.Text()),
 			tui.NewSpacer(),
 		))
-		input.SetText("")
+		chatView.Input.SetText("")
 	})
 
-	root := tui.NewHBox(chat)
+	root := tui.NewHBox(chatView.Chat)
 	ui, err := tui.New(root)
-	for i := 0; i < 10; i++{
-		history.Append(tui.NewHBox(
-			tui.NewLabel(time.Now().Format("15:04")),
-			tui.NewPadder(1, 0, tui.NewLabel(fmt.Sprintf("<%s>", "john"))),
-			tui.NewLabel("message"),
-			tui.NewSpacer(),
-		))
-	}
+	
 	if err != nil {
 		log.Fatal(err)
 	}
