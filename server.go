d64d0bba5159978d25bab92249a9d88154faade1
Eduardo Colina <eacolina@edu.uwaterloo.ca>
2019-12-07 00:36:52 -0500

    Added support for multiple connections

--- server.go
+++ server.go
@@ -8,34 +8,54 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+
+type message struct {
+	Sender string
+	Receiver string
+	Content string
+
+}
+
 var port = *flag.String("ip", "3434", "help message for flagname")
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
+var connections = make(map [string]*websocket.Conn)
+
+func listenForMessages(conn *websocket.Conn, sender string){
+	for{
+		message := message{}
+		err := conn.ReadJSON(&message)
+		if err != nil{
+			panic(err)
+		}
+		fmt.Printf("Received messaged: `%s` from %s to %s\n", message.Content, sender, message.Receiver)
+		message.Sender = sender
+		connections[message.Receiver].WriteJSON(message)
+	}
+}
 
 func main() {
 	flag.Parse()
-	fmt.Println("Starting server...")
+
+	fmt.Println("Starting server... 🚀")
 	handler := func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Server", "A Go Server")
 		w.Write([]byte("Hello\n"))
 	}
 	ws_handler := func(w http.ResponseWriter, r *http.Request) {
 		upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+		sender := r.Header.Get("sender")
+
 		conn, err := upgrader.Upgrade(w, r, nil)
+		connections[sender] = conn
+		go listenForMessages(conn, sender)
 		if err != nil {
 			panic(err)
 		}
-		for {
-			_, p, err := conn.ReadMessage()
-			if err != nil{
-				break
-			}
-			fmt.Printf("Received messaged: %s\n", p)
-			conn.WriteMessage(websocket.TextMessage, []byte("You sir have been aknowledged"))
-		}
+		fmt.Printf("Done with connection for %s", sender)
 	}
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/ws", ws_handler)
